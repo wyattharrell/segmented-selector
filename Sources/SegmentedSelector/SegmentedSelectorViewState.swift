f46aaf9bde81120405d501833c6e54862cec422b
Wyatt Harrell <wyattaharrell@gmail.com>
2024-12-17 23:30:34 -0500

    Create SegmentedSelectorViewState.swift

--- Sources/SegmentedSelector/SegmentedSelectorViewState.swift
+++ Sources/SegmentedSelector/SegmentedSelectorViewState.swift
@@ -1,8 +1,44 @@
 //
-//  File.swift
+//  SegmentedSelectorViewState.swift
 //  SegmentedSelector
 //
 //  Created by Wyatt Harrell on 12/17/24.
 //
 
-import Foundation
+import SwiftUI
+
+public typealias CRI = CaseIterable & RawRepresentable & Identifiable
+
+public final class SegmentedSelectorViewState<T: CRI>: ObservableObject where T.RawValue == String, T.AllCases: RandomAccessCollection {
+
+    var font: Font
+    var animation: Animation
+    var cornerRadius: CGFloat
+    var padding: CGFloat
+    var selectedSegmentColor: Color
+    var backgroundColor: Color
+
+    @Published public var selectedSegment: T
+
+    init(
+        selectedSegmentColor: Color = Color.white,
+        backgroundColor: Color = Color(UIColor.systemGray6),
+        font: Font = Font.system(
+            .body,
+            design: .rounded,
+            weight: .medium
+        ),
+        animation: Animation = .default,
+        cornerRadius: CGFloat = 10,
+        padding: CGFloat = 5,
+        selectedSegment: T
+    ) {
+        self.font = font
+        self.animation = animation
+        self.cornerRadius = cornerRadius
+        self.padding = padding
+        self.selectedSegment = selectedSegment
+        self.selectedSegmentColor = selectedSegmentColor
+        self.backgroundColor = backgroundColor
+    }
+}
